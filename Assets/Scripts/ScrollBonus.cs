212e6c6f6a51088bbf807a71ed9238b79800a186
EberthFelipe <eberthfelipe@gmail.com>
2017-03-25 16:32:54 -0400

    starting bonus dev

---
+++ Assets/Scripts/ScrollBonus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBonus : MonoBehaviour {
+
+    [Header("Velocity")]
+    public float bonusVelocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        //set bonus initial position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * bonusVelocity * Time.deltaTime;
+    }
+}
